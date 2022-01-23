60e4b02b0efad2065b21c9a6bfb761dbf504e198
thomps0189 <angelathompson2011@gmail.com>
2022-01-23 11:17:45 -0600

    server and connection .js

--- config/connection.js
+++ config/connection.js
@@ -1,1 +1,12 @@
-js
+const Sequelize = require('sequelize');
+
+require('dotenv').config();
+
+// create connection to db, mysql login info
+const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
+    host: 'localhost',
+    dialect: 'mysql',
+    port: 3306
+});
+
+module.export = sequelize
